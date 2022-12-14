01f50ca684cbb69c99738b703b5a139fe2fb8273
bonnefoi marion <marion-b-13@live.fr>
2022-12-14 15:32:49 +0100

    modificiation

--- nav.js
+++ nav.js
@@ -7,3 +7,15 @@ function closeMenu(){
   console.log(menuMobile);
   menuMobile.style.transform = 'translate(-150%)';
 }
+function openMenu(){
+  let case1 = document.getElementsByClassName('checkbox')[0];
+
+  let menuMobile = document.getElementsByClassName('menu-items')[0];
+  if(case1.checked = true){
+    closeMenu()
+  }else{
+    case1.checked = true;
+    menuMobile.style.transform = 'translate(0%)';
+    console.log('test');
+  }
+}
